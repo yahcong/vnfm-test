bb03f521aac5d1d5d3043b5d6cd0dd1edf148120
yahcong <congyahuan@ebupt.com>
2017-09-13 07:48:43 +0000

    add framework of vnfm-test
    Signed-off-by: yahcong <congyahuan@ebupt.com>

---
+++ lib/vnfm.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+. ../lib/log.sh
+
+NETWORK_CONFIG=
+readonly VNFD_PATH=
+
+
+# @age: check_vm
+# @description:
+# @param1: 
+# @param2: 
+# @param...
+check_vm()
+{
+
+}
+
+
+# @age: check_flavor
+# @description: 
+check_flavor()
+{
+
+}
+
+
+# @age: check_image
+# @description: 
+check_image()
+{
+
+}
+
+
+# @age: check_ip
+# @description: 
+check_ip()
+{
+
+}
+
+
+# @age: check_volume
+# @description: 
+check_volume()
+{
+
+}
+
+
+# @age: check_personality
+# @description: 
+check_personality()
+{
+
+}
+
+
+# @age: check_database
+# @description: 
+check_database()
+{
+
+}
+
+
+# @age: get_vnf
+# @description: 
+get_vnf()
+{
+}
